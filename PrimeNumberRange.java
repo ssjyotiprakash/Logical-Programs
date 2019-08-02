9b091ef59640d55469a583e96db04a7fa4424882
ssjyotiprakash <49389953+ssjyotiprakash@users.noreply.github.com>
2019-08-02 10:01:19 +0530

    Add files via upload

---
+++ PrimeNumberRange.java
@@ -0,0 +1,18 @@
+class PrimeNumberRange{
+public static void main(String[] args){
+int temp=0;
+for(int i=1;i<=100;i++){
+    for(int j=2;j<=i-1;j++){
+       if(i%j==0){
+          temp=temp+1;
+       }
+    }
+    if(temp==0){
+       System.out.println(i);
+    }
+     else{
+       temp=0;
+     }
+}
+  }
+}
